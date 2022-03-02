639c2cf1a58e2646f47f8ec4a18434217364fb46
Americo Meneses <beatamez@hotmail.com>
2022-03-01 19:14:48 -0600

    Reading notes

--- NotesApp/Controllers/ListNoteTableViewController.swift
+++ NotesApp/Controllers/ListNoteTableViewController.swift
@@ -7,23 +7,45 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 class ListNoteTableViewController: UITableViewController {
     
     let manager = CoreDataStack()
+    var notesArray = [Note]()
     
-    var notes = ["Note one","Note two","Note three","Note Four"]
     var selectedCategory: Category? {
         didSet {
-//            loadNotes()
+            loadNotes()
             //            print(selectedCategory)
         }
     }
     
     @IBOutlet var listNotesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("listNotesviewWillDidLoad")
         listNotesTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+        print("listNotesviewWillAppear")
+    }
+    
+    public func loadNotes() {
+        let request: NSFetchRequest<Note> = Note.fetchRequest()
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        request.predicate = categoryPredicate
+        
+        do {
+            notesArray = try manager.context.fetch(request)
+        } catch {
+            print("Error fetching notes \(error.localizedDescription)")
+        }
     }
     
     @IBAction func onAddBtnPressed(_ sender: UIBarButtonItem) {
@@ -36,12 +58,12 @@ class ListNoteTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return notes.count
+        return notesArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = notes[indexPath.row]
+        cell.textLabel?.text = notesArray[indexPath.row].descriptionText
         return cell
     }
 }
